dae9c85080965ae016c82aae3f1ab97bb6b4efd1
Walter Casadiegos <casadiegosw@gmail.com>
2026-04-09 20:30:57 -0500

    detectar IP automaticamente para dispositivos moviles

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,21 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from 'react-native';
+import Constants from 'expo-constants';
 
 const TOKEN_KEY = "@p2p_token";
-
-// Detectar plataforma y usar la URL apropiada
 
 const getBaseURL = () => {
   if (Platform.OS === 'web') {
     return "http://localhost:8080";
   }
-  if (Platform.OS === 'android') {
-    return "http://10.0.2.2:8080"; // Android emulator
+
+  const expoHost = Constants.expoConfig?.hostUri?.split(':').shift();
+  if (expoHost) {
+    return `http://${expoHost}:8080`;
   }
-  return "http://localhost:8080"; // iOS simulator
+
+  return "http://localhost:8080";
 };
 
 export const api = axios.create({
